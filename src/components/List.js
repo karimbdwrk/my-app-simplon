ed892ddbebe384798feb581c6f5fd6d8b71a8eab
Karim Badwork <karim@badwork.fr>
2023-10-25 14:49:25 +0200

    add styles

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,136 @@
 import React from "react";
 
+import TitleH1 from "./TitleH1";
+
+const data = [
+	{
+		name: "Pomme",
+		type: "Fruit",
+		color: "Rouge",
+	},
+	{
+		name: "Banane",
+		type: "Fruit",
+		color: "Jaune",
+	},
+	{
+		name: "Orange",
+		type: "Fruit",
+		color: "Orange",
+	},
+	{
+		name: "Kiwi",
+		type: "Fruit",
+		color: "Vert",
+	},
+	{
+		name: "Fraise",
+		type: "Fruit",
+		color: "Rouge",
+	},
+	{
+		name: "Raisin",
+		type: "Fruit",
+		color: "Violet",
+	},
+	{
+		name: "Pêche",
+		type: "Fruit",
+		color: "Orange",
+	},
+	{
+		name: "Mangue",
+		type: "Fruit",
+		color: "Jaune",
+	},
+	{
+		name: "Abricot",
+		type: "Fruit",
+		color: "Orange",
+	},
+	{
+		name: "Ananas",
+		type: "Fruit",
+		color: "Jaune",
+	},
+	{
+		name: "Carotte",
+		type: "Légume",
+		color: "Orange",
+	},
+	{
+		name: "Tomate",
+		type: "Légume",
+		color: "Rouge",
+	},
+	{
+		name: "Brocoli",
+		type: "Légume",
+		color: "Vert",
+	},
+	{
+		name: "Épinard",
+		type: "Légume",
+		color: "Vert",
+	},
+	{
+		name: "Courgette",
+		type: "Légume",
+		color: "Vert",
+	},
+	{
+		name: "Aubergine",
+		type: "Légume",
+		color: "Violet",
+	},
+	{
+		name: "Poivron",
+		type: "Légume",
+		color: "Rouge, Jaune, Vert",
+	},
+	{
+		name: "Concombre",
+		type: "Légume",
+		color: "Vert",
+	},
+	{
+		name: "Patate douce",
+		type: "Légume",
+		color: "Orange",
+	},
+	{
+		name: "Oignon",
+		type: "Légume",
+		color: "Blanc",
+	},
+	{
+		name: "Laitue",
+		type: "Légume",
+		color: "Vert",
+	},
+];
+
 const List = () => {
-	return <p>list</p>;
+	return (
+		<div>
+			<TitleH1 name='Karim' age={33} />
+			{data.map((item) => (
+				<div style={styles.card}>
+					<p>
+						{item.name} - {item.type} - {item.color}
+					</p>
+				</div>
+			))}
+		</div>
+	);
+};
+
+const styles = {
+	card: {
+		backgroundColor: "pink",
+		border: "1px solid #000",
+		color: "#101010",
+	},
 };
 
 export default List;
