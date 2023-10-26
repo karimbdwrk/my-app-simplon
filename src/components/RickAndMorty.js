164ec62dfc71f80498c8063fd65b5559276a7cbf
Karim Badwork <karim@badwork.fr>
2023-10-26 11:55:51 +0200

    image RM

--- src/components/RickAndMorty.js
+++ src/components/RickAndMorty.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 const RickAndMorty = () => {
-	const [data, setData] = useState({});
+	const [data, setData] = useState([]);
+	const [filteredData, setFilteredData] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
@@ -13,7 +14,8 @@ const RickAndMorty = () => {
 					"https://rickandmortyapi.com/api/character"
 				);
 				const dataJson = await response.json();
-				setData(dataJson);
+				setData(dataJson.results);
+				setFilteredData(dataJson.results);
 				setLoading(false);
 			} catch (error) {
 				console.error("Erreur lors de l'appel de l'API :", error);
@@ -22,9 +24,22 @@ const RickAndMorty = () => {
 		};
 
 		fetchData();
-
-		console.log(data);
 	}, []);
+
+	// useEffect(() => {
+	// 	console.log(filteredData);
+	// }, [filteredData]);
+
+	const handleFilter = (filter) => {
+		const filteredCharacters = data.filter(
+			(character) => character.status === filter
+		);
+		setFilteredData(filteredCharacters);
+	};
+
+	const handleReset = () => {
+		setFilteredData(data);
+	};
 
 	return (
 		<div>
@@ -34,19 +49,36 @@ const RickAndMorty = () => {
 			) : (
 				<div>
 					<div>
-						<button>Alive</button>
-						<button>Dead</button>
-						<button>Reset</button>
+						<button onClick={() => handleFilter("Alive")}>
+							Alive
+						</button>
+						<button onClick={() => handleFilter("Dead")}>
+							Dead
+						</button>
+						<button onClick={handleReset}>Reset</button>
 					</div>
-					{data.results.map((character) => (
-						<p key={character.id}>
-							{character.name} - {character.status}
-						</p>
+					{filteredData.map((character) => (
+						<div style={styles.card} key={character.id}>
+							<img src={character.image} />
+							{/* <Image src={character.image} /> */}
+							<p>
+								{character.name} - {character.status}
+							</p>
+						</div>
 					))}
 				</div>
 			)}
 		</div>
 	);
+};
+
+const styles = {
+	card: {
+		border: "1px solid #F7F7F7",
+		borderRadius: 5,
+		margin: 15,
+		padding: 15,
+	},
 };
 
 export default RickAndMorty;
